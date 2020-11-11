be8b220546f4eb9ca63655d938263d2586169114
SkyWorldMC <73902956+SkyWorldMC@users.noreply.github.com>
2020-11-11 10:57:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     "token": "process.env.Nzc1NzQ3NTQ2NzY4MzQzMDkw.X6q1Mw.3atUjRFbQ3wzv5yHJKUC-mIMla0",
     "prefix": "process.env.sky!",
-    "owners": ["process.env.660386189819510828]",
-    "host": process.env.HOST,
+    "owners": "[process.env.660386189819510828]",
+    "host": "process.env.https://skyworldmoderation.herokuapp.com",
     "hostpassw": process.env.HOSTPASSW
 }
